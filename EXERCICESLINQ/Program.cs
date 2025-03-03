d7ff377194aa0fcc152b8e04bb999b483846726a
Israël TODOME <tdonanon@outlook.fr>
2025-03-03 14:23:30 -0500

    Dernière mise à jour

--- EXERCICESLINQ/Program.cs
+++ EXERCICESLINQ/Program.cs
@@ -11,7 +11,7 @@ namespace EXERCICESLINQ
     {
         static void Main(string[] args)
         {
-            int[] tab = new[] { 1, 2, 3, 4, 5 };
+            int[] tab = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             System.Console.WriteLine(tab.CompterPair());
         }
     }
